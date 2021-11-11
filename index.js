ce1eebcf6ca5dd9fb4cc5bbd3276d7698c809f49
Celdama <celdama.dev@gmail.com>
2021-11-11 15:35:57 +0100

    added function and logic to allow user will advance on previous slide accodingly

--- index.js
+++ index.js
@@ -7,13 +7,34 @@ const slider = (() => {
   ];
 
   const previousWithEffect = () => {
+    const firstImg = document.querySelector('.image');
+    const previousImg = document.querySelector('.image0');
 
+    const fullPath = firstImg.src;
+    const filename = fullPath.replace(/^.*[\\\/]/, '');
+    const indexOfCurrentImg = images.indexOf(filename);
+
+    if (indexOfCurrentImg >= 0) {
+      let indexOfPreviousImage = indexOfCurrentImg - 1;
+      if (indexOfPreviousImage < 0) {
+        indexOfPreviousImage = 3;
+      }
+
+      const previousFileName = images[indexOfPreviousImage];
+      const previousImageFullPath = `./images/${previousFileName}`;
+      previousImg.src = `${previousImageFullPath}`;
+      previousImg.classList.add('active-previous');
+
+      setTimeout(() => {
+        firstImg.src = previousImg.src;
+        previousImg.classList.remove('active-previous');
+      }, (1500));
+    }
   };
 
   const nextWithEffect = () => {
     const firstImg = document.querySelector('.image');
     const secondImg = document.querySelector('.image2');
-    console.log(secondImg);
 
     const fullPath = firstImg.src;
     const filename = fullPath.replace(/^.*[\\\/]/, '');
@@ -28,23 +49,24 @@ const slider = (() => {
       const nextImageFileName = images[indexOfNextImage];
       const nextImageFullPath = `./images/${nextImageFileName}`;
       secondImg.src = `${nextImageFullPath}`;
-      secondImg.classList.add('active');
+      secondImg.classList.add('active-next');
 
       setTimeout(() => {
         firstImg.src = secondImg.src;
-        secondImg.classList.remove('active');
+        secondImg.classList.remove('active-next');
       }, 1500);
     }
   };
 
   const animateSlider = () => {
-    // images.forEach((image) => {
-    //   console.log(image);
-    // });
-
     const btn = document.querySelector('.next');
     btn.addEventListener('click', () => {
       nextWithEffect();
+    });
+
+    const previousBtn = document.querySelector('.previous');
+    previousBtn.addEventListener('click', () => {
+      previousWithEffect();
     });
   };
 
